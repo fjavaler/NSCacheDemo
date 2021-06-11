ddfcfa46e9e379958c332e1c17ca6a4161ec22fd
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-10 18:14:52 -0600

    Initial Commit

---
+++ NSCacheDemo/NSCacheDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NSCacheDemoApp.swift
+//  NSCacheDemo
+//
+//  Created by Fred Javalera on 6/10/21.
+//
+
+import SwiftUI
+
+@main
+struct NSCacheDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
